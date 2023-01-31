74a55de6437140f2840b88413839f2a305623110
DrTim105 <Salihutimothy52@gmail.com>
2023-02-01 00:27:15 +0100

    displayed sample widow data in piechart

--- app/src/main/java/com/salihutimothy/mobiledevgraphapp/MainActivity.kt
+++ app/src/main/java/com/salihutimothy/mobiledevgraphapp/MainActivity.kt
@@ -4,10 +4,27 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import android.view.View
+
+import com.anychart.chart.common.dataentry.ValueDataEntry
+
+import com.anychart.chart.common.dataentry.DataEntry
+
+import com.anychart.charts.Pie
+
+
+
+//import com.anychart.DataEntry;
+//import com.anychart.Pie;
+//import com.anychart.ValueDataEntry;
 
 
 class MainActivity : AppCompatActivity(), GetJsonData.OnDownloadComplete, ParseJsonData.OnDataAvailable {
     private val TAG = "MainActivity"
+
+    private lateinit var widowEmploymentStatus: AnyChartView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +33,7 @@ class MainActivity : AppCompatActivity(), GetJsonData.OnDownloadComplete, ParseJ
         val getJsonData = GetJsonData(this)
         getJsonData.execute(this)
 
+        widowEmploymentStatus = findViewById(R.id.widowEmploymentStatus)
 
     }
 
@@ -33,6 +51,30 @@ class MainActivity : AppCompatActivity(), GetJsonData.OnDownloadComplete, ParseJ
 
     override fun onDataAvailable(data: List<Widow>) {
         Log.d(TAG, ".onDataAvailable called ${data[0]}")
+
+        widowEmploymentStatus = findViewById(R.id.widowEmploymentStatus)
+
+        var withNgo = 0
+        var withoutNgo = 0
+
+        for (widow in data) {
+            if (widow.ngoName == null) {
+                withoutNgo++
+            } else {
+                withNgo++
+            }
+        }
+        val pie = AnyChart.pie()
+
+        val pieData: MutableList<DataEntry> = ArrayList()
+        pieData.add(ValueDataEntry("Yes", withNgo))
+        pieData.add(ValueDataEntry("No", withoutNgo))
+
+        pie.data(pieData)
+        pie.palette(arrayOf("#039CDD", "#602BF8"))
+//        val anyChartView = findViewById<View>(R.id.widowEmploymentStatus) as AnyChartView
+        widowEmploymentStatus.setChart(pie)
+
     }
 
     override fun onError(exception: Exception) {
